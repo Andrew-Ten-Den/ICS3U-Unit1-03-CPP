959f669a0d999ee662a8b5f883342a192344e35d
Andrew-Ten-Den <andrew.ten-den@mths.ca>
2022-02-18 13:38:12 +0000

    added FinalHelloWorld

---
+++ FinalHelloWorld.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Andrew-Ten-Den All rights reserved
+//
+// Created by: Andrew-Ten-Den
+// Created on: February 2022
+// This is the "Final hello world" program, with proper style
+
+#include <iostream>
+
+int main() {
+std::cout << "Final hello world";
+}
